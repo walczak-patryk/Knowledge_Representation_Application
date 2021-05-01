358ef726ddb5ed3ed6561ac645c1703e5076f469
martin <maartinmxxx@gmail.com>
2021-05-01 13:35:49 +0200

    Initial GUI setup

--- KnowledgeRepresentationInterface/MainWindow.xaml.cs
+++ KnowledgeRepresentationInterface/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -23,6 +24,23 @@ namespace KnowledgeRepresentationInterface
         public MainWindow()
         {
             InitializeComponent();
+            TreeViewItem t = new TreeViewItem();
+            t.Header = "Fluent II";
+            TreeViewItem t2 = new TreeViewItem();
+            t2.Header = "Option";
+            t.Items.Add(t2);
+            Fluents_TreeViewItem.Items.Add(t);
+            for(int i=0;i<50;i++)
+            {
+                TreeViewItem t3 = new TreeViewItem();
+                t3.Header = "Fluent " + i.ToString();
+                Fluents_TreeViewItem.Items.Add(t3);
+            }
+        }
+
+        public double Test2()
+        {
+            return ActualWidth / 2;
         }
     }
 }
