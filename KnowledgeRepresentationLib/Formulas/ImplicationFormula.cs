13187f6ab443eadb808b792771236d83b08eb556
unknown <kacper.gasior@gmail.com>
2021-05-11 16:21:36 +0200

    Add formulas

---
+++ KnowledgeRepresentationLib/Formulas/ImplicationFormula.cs
@@ -0,0 +1,18 @@
+﻿namespace KR_Lib.Formulas
+{
+    public class ImplicationFormula : IFormula
+    {
+        IFormula formula;
+        IFormula formula2;
+
+        public ImplicationFormula(IFormula formula, IFormula formula2)
+        {
+            this.formula = formula;
+            this.formula2 = formula2;
+        }
+        public bool Evaluate()
+        {
+            return !this.formula.Evaluate() | this.formula2.Evaluate();
+        }
+    }
+}
