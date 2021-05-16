6537fc9617d31b1c9b38c1362d15430ba6bd8776
Kornel Mrozowski <kpmrozowski@gmail.com>
2021-05-16 15:47:34 +0200

    Scenarios v1.0

--- KnowledgeRepresentationLib/Scenarios/Observations.cs
+++ KnowledgeRepresentationLib/Scenarios/Observations.cs
@@ -1,15 +1,47 @@
-﻿using KR_Lib.DataStructures;
-using KR_Lib.Formulas;
+﻿using KR_Lib.Formulas;
+using KR_Lib.Tree;
 
-namespace KnowledgeRepresentationLib.Scenarios
+using System;
+
+namespace KR_Lib.Scenarios
 {
     public class Observations
     {
-        public void MetodaPoglądowa() 
+        public Guid Id { get; set; }
+        string Name { get; set; }
+        IFormula Form { get; set; }
+        public int Time { get; set; }
+        public Observations(string name, IFormula formula, int time) : base()
         {
-            IFormula formula = new Formula(new Fluent("loaded", false));
-            formula = new NegationFormula(formula);
-            formula.Evaluate();
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+            this.Form = formula;
+            this.Time = time;
         }
+
+        /// <summary>
+        /// We check if all fluents of parameter state are equal to matching fluents stored in this object.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public bool CheckState(State state, int time)
+        {
+            if (!time.Equals(Time))
+            {
+                return true;
+            }
+            return Form.Evaluate(state);
+        }
+        public override string ToString()
+        {
+            return "Expression " + Form.ToString() + " in time " + Time.ToString();
+        }
+        //public void MetodaPoglądowa() 
+        //{
+        //    IFormula formula = new Formula(new Fluent("loaded", false));
+        //    formula = new NegationFormula(formula);
+        //    formula.Evaluate();
+        //}
     }
 }
